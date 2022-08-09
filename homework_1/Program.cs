73d80476f3b3213fac541b9d6109a4b97f2d22e6
Константин Королев <konstantinkorolev@MacBook-Pro-Konstantin.local>
2022-08-09 21:58:20 +0600

    task_1

---
+++ homework_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = new Random().Next(1,25);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,25);
+Console.WriteLine(numberB);
+int max = numberA;
+if (numberA>max) max = numberA;
+if (numberB>max) max = numberB;
+
+Console. Write("max = ");
+Console.WriteLine(max);
