4f0426f6ddc1e3e59d45f0b2acf289aaf6d9128a
Константин Королев <konstantinkorolev@MacBook-Pro-Konstantin.local>
2022-08-10 20:42:26 +0600

    task 4

---
+++ homework_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int N;
+
+Console.Write("Введите число : ");
+N = Convert.ToInt32(Console.ReadLine());
+for (int x = 1; x <= N; x++)
+
+if(x % 2 == 0 )
+{
+    Console.WriteLine(x);
+    Console.WriteLine("четное");
+}
+    
+
+
